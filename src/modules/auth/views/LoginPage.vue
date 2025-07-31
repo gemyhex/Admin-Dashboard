2bcccf6918deb9d9c515e2af1904e8934ae0f9e1
Ahmed Gamal <a.abdrapo@wakeb.tech>
2025-07-31 06:31:45 +0400

    refactor: some enhancements in the core of the dashboard

--- src/modules/auth/views/LoginPage.vue
+++ src/modules/auth/views/LoginPage.vue
@@ -1,8 +1,8 @@
 <template>
   <AuthLayout>
-    <div class="w-screen min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-800 to-purple-900 px-4">
+    <div class="w-screen min-h-screen flex items-center justify-center bg-gradient-to-br from-[#820235] to-[#310756] px-4">
       <div class="w-full max-w-md backdrop-blur-lg bg-white/10 dark:bg-white/10 border border-white/20 rounded-xl shadow-xl p-8 space-y-6">
-        <h2 class="text-center text-2xl font-bold text-white">
+        <h2 class="text-center text-5xl font-bold text-white">
           {{ appName }}
         </h2>
         <LoginForm />
