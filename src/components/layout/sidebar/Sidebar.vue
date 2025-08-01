7dbb9930886cf0580ec9a964efd8815a64bab14f
Ahmed Gamal <a.abdrapo@wakeb.tech>
2025-08-01 06:20:14 +0400

    refactor: some enhancement

--- src/components/layout/sidebar/Sidebar.vue
+++ src/components/layout/sidebar/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <aside class="w-56 bg-white dark:bg-gray-800 min-h-screen shadow pt-6">
     <h1 class="text-xl font-semibold dark:text-white text-center">Rezilens Dashboard</h1>
-    <nav class="flex flex-col p-4 mt-6 bg-white ">
+    <nav class="flex flex-col p-4 mt-6 ">
       <SidebarLink to="/dashboard">Dashboard</SidebarLink>
       <SidebarLink to="/users">Users Management</SidebarLink>
       <SidebarLink to="/reports">Reports</SidebarLink>
